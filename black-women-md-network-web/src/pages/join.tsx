c0fa89c87ed043926b0fcb57b988f5067d0cb059
pdeveau1 <pdeveau@bu.edu>
2022-10-31 12:17:59 -0400

    Add files for join page and sign up page

---
+++ black-women-md-network-web/src/pages/join.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+  
+const Join = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Right',
+        alignItems: 'Right',
+        height: '100vh'
+      }}
+    >
+      <h1>Membership</h1>
+    </div>
+  );
+};
+  
+export default Join;
