70f2e1bdae59683be73e9b251a4f8263d7fd1271
pdeveau1 <pdeveau@bu.edu>
2022-10-30 22:31:06 -0400

    https://www.geeksforgeeks.org/create-a-responsive-navbar-using-reactjs/ used this reference to create navbar component

--- black-women-md-network-web/src/App.tsx
+++ black-women-md-network-web/src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Welcome from './pages/welcome';
+import Membership from './pages/membership';
+import Resources from './pages/resources';
+import Community from './pages/community';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Welcome />} />
+        <Route path='/membership' element={<Membership />} />
+        <Route path='/resources' element={<Resources />} />
+        <Route path='/community' element={<Community />} />
+      </Routes>
+    </Router>
   );
 }
-
+  
 export default App;
---
+++ black-women-md-network-web/src/pages/welcome.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+  
+const Welcome = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Right',
+        alignItems: 'Right',
+        height: '100vh'
+      }}
+    >
+      <h1>Welcome to Black Women MD Network</h1>
+    </div>
+  );
+};
+  
+export default Welcome;
