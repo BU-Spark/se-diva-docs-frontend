d7c127b59b426d99b5f5a9d38d63e0c3d83a62f8
pdeveau1 <pdeveau@bu.edu>
2022-11-07 10:49:04 -0500

    comment out logo

--- black-women-md-network-web/src/components/Navbar/index.tsx
+++ black-women-md-network-web/src/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        {/*<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>*/}
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
