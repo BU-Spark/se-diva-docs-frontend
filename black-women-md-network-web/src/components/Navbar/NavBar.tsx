d6205963688144409105fc44b88ed538992662d4
pdeveau1 <pdeveau@bu.edu>
2022-11-07 13:05:30 -0500

    add css file to start modifying design

--- black-women-md-network-web/src/components/Navbar/NavBar.tsx
+++ black-women-md-network-web/src/components/Navbar/NavBar.tsx
@@ -1,41 +1,51 @@
 import React from 'react';
+import './NavBar.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Col from 'react-bootstrap/Col'
 
 function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
+    <Navbar className="navbar" variant="dark">
+      <Container fluid>
         {/*<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>*/}
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <NavDropdown title="Membership" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="/joinus">Join Us!</NavDropdown.Item>
+          <Nav className="nav">
+          <Col>
+            <NavDropdown className="navdropdown" title="Membership" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="/join">Join Us!</NavDropdown.Item>
               <NavDropdown.Item href="/directory">Directory</NavDropdown.Item>
               <NavDropdown.Item href="/resumebank">Resume Bank</NavDropdown.Item>
               <NavDropdown.Item href="/findanexpert">Find An Expert</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Resources" id="collasible-nav-dropdown">
+          </Col>
+          <Col>
+            <NavDropdown className="navdropdown" title="Resources" id="collasible-nav-dropdown">
               <NavDropdown.Item href="/resumereview">Resume Review</NavDropdown.Item>
               <NavDropdown.Item href="/headshot">Headshot</NavDropdown.Item>
               <NavDropdown.Item href="/findanexpert">Find An Expert</NavDropdown.Item>
               <NavDropdown.Item href="/coaching">Coaching</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Events" id="collasible-nav-dropdown">
+          </Col>
+          <Col>
+            <NavDropdown className="navdropdown" title="Events" id="collasible-nav-dropdown">
             </NavDropdown>
-            <NavDropdown title="Community" id="collasible-nav-dropdown">
+          </Col>
+          <Col>
+            <NavDropdown className="navdropdown" title="Community" id="collasible-nav-dropdown">
               <NavDropdown.Item href="https://www.divadocsboston.com/">Diva Docs</NavDropdown.Item>
               <NavDropdown.Item href="/events">Events</NavDropdown.Item>
             </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+          </Col>
+          <Col>
+            <Nav.Link href="/signin">Sign In</Nav.Link>
+          </Col>
+          <Col>
+            <Nav.Link href="/join">Join</Nav.Link>
+          </Col>
           </Nav>
         </Navbar.Collapse>
       </Container>
