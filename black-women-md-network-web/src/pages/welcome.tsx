26f4ad11410b6d26bd2615b4f8d37f2d949332e6
pdeveau1 <pdeveau@bu.edu>
2022-11-08 09:29:38 -0500

    change featured member to right name and remove pointer for button

--- black-women-md-network-web/src/pages/welcome.tsx
+++ black-women-md-network-web/src/pages/welcome.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import buddies from '../images/test.jpg';
+import explain from '../images/blackwomanexplains.jpg';
 import './welcome.css';
 import styled from 'styled-components';
 
@@ -8,8 +9,13 @@ import styled from 'styled-components';
 const Welcome = () => {
   return (
     <>
-      <img src = {buddies} alt = "Freedom Blog" />
-      <button>Join</button>
+      <img className="img-buddies" src = {buddies} alt = "Black Woman Buddies" />
+      <a href="/join" className="btn btn-join btn-lg active" role="button" aria-pressed="true">Join Our Community</a>
+      <p className="p-welcome">Diva Docs Boston is an organization composed of over 250 Black women physicians and 
+      trainees (medical students, residents, and fellows) working, studying or living in Greater Boston. Our members 
+      are represented in every medical speciality and work in a variety of health and non-health care settings.</p>
+      <a href="" className="btn btn-members btn-lg active" role="button" aria-pressed="true">Featured Members</a>
+      <img className="img-explain" src = {explain} alt = "Black Woman Explains" />
     </>
   );
 }
