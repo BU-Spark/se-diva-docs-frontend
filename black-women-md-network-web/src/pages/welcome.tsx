ff9bec47304694d1f36cc4e7c934c9c9adcd35eb
Mya Turner <68873697+MyaTurner@users.noreply.github.com>
2022-11-08 15:37:52 -0500

    got rid of styled-components

--- black-women-md-network-web/src/pages/welcome.tsx
+++ black-women-md-network-web/src/pages/welcome.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import buddies from '../images/test.jpg';
-import './welcome.css';
-import styled from 'styled-components';
+import React from "react";
+import buddies from "../images/test.jpg";
+import "./welcome.css";
 
 const Welcome = () => {
-  return (
-    <img src = {buddies} alt = "Freedom Blog" />
-  )
-}
-  
+  return <img src={buddies} alt="Freedom Blog" />;
+};
+
 export default Welcome;
