c356286eb0cb9924430b67260fc01b5fe0e3b045
Mya Turner <68873697+MyaTurner@users.noreply.github.com>
2022-11-09 10:02:36 -0500

    trying to pass states from one componeent to another

--- black-women-md-network-web/src/pages/MembershipApplication/ContactInfo.tsx
+++ black-women-md-network-web/src/pages/MembershipApplication/ContactInfo.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 
-const ContactInfo = () => {
+const ContactInfo = (
+  name: any,
+  setName: React.Dispatch<React.SetStateAction<string>>
+) => {
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`);
+  };
   return (
     <div>
       <h1> Contact / General Information </h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter your name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <input type="submit" />
+      </form>
     </div>
   );
 };
--- black-women-md-network-web/src/pages/MembershipApplication/join.tsx
+++ black-women-md-network-web/src/pages/MembershipApplication/join.tsx
@@ -1,17 +1,53 @@
 import React, { useState } from "react";
 import ContactInfo from "./ContactInfo";
+import FinalQuestions from "./FinalQuestions";
+import MembershipType from "./MembershipType";
+import Success from "./Success";
 
 const Join = () => {
-  return (
-    <div>
-      <h1>Membership Application</h1>
-      <h2>
-        Thank you for your interest in becoming a member of the Black Women's MD
-        Network!
-      </h2>
-      <ContactInfo></ContactInfo>
-    </div>
-  );
+  const [step, setStep] = useState(1);
+  // const [values, setValues] = useState({
+  //   firstName: "",
+  //   midName : "",
+  //   lastName: ""
+  // })
+  const [name, setName] = useState("Paul");
+
+  // Proceed to next step
+  const nextStep = () => {
+    if (step < 3) {
+      setStep(step + 1);
+    }
+  };
+
+  // Go to previous step
+  const prevStep = () => {
+    if (step > 1) {
+      setStep(step + 1);
+    }
+  };
+
+  // const handleChange = (event,) => {
+  //   const name = event.target.name;
+  //   const value = event.target.value;
+  //   setValues(values => ({...values, [name]: value}))
+  // }
+
+  switch (step) {
+    case 1:
+      return <ContactInfo name={name} setName={setName}></ContactInfo>;
+
+    case 2:
+      return <MembershipType></MembershipType>;
+
+    case 3:
+      return <FinalQuestions></FinalQuestions>;
+
+    case 4:
+      return <Success></Success>;
+    default:
+      return <ContactInfo></ContactInfo>;
+  }
 };
 
 export default Join;
