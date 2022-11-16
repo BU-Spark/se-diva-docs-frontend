c968a7afc35ef5f31a60c94c7b546c0e11c6ea54
pdeveau1 <pdeveau@bu.edu>
2022-11-16 12:01:54 -0500

    Update links to match changes from Philomena

--- black-women-md-network-web/src/components/Footer/Footer.tsx
+++ black-women-md-network-web/src/components/Footer/Footer.tsx
@@ -4,29 +4,30 @@ const Footer = (): JSX.Element => <footer className="page-footer font-small blue
     <div className="container-fluid text-center text-md-left">
         <div className="row">
             <div className="col-md-6 mt-md-0 mt-3">
-                <h5 className="text-uppercase">Footer Content</h5>
+                <h5 className="text-uppercase">Black Women M.D. Network</h5>
                 <p>Here you can use rows and columns to organize your footer content.</p>
             </div>
 
             <hr className="clearfix w-100 d-md-none pb-0"/>
 
             <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">Links</h5>
+                <h5 className="text-uppercase">JOIN Us!</h5>
                 <ul className="list-unstyled">
-                    <li><a href="#!">Link 1</a></li>
-                    <li><a href="#!">Link 2</a></li>
-                    <li><a href="#!">Link 3</a></li>
-                    <li><a href="#!">Link 4</a></li>
+                    <li><a href="#!">Subscriptions & Dues</a></li>
+                    <li><a href="#!">Become A Member</a></li>
+                    <li><a href="#!">Become A Partner</a></li>
+                    <li><a href="#!">Become A Sponsor</a></li>
                 </ul>
             </div>
 
             <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">Links</h5>
+                <h5 className="text-uppercase">CONTACT US!</h5>
                 <ul className="list-unstyled">
-                    <li><a href="#!">Link 1</a></li>
-                    <li><a href="#!">Link 2</a></li>
-                    <li><a href="#!">Link 3</a></li>
-                    <li><a href="#!">Link 4</a></li>
+                    <li><a href="#!">General Inquiries</a></li>
+                    <li><a href="#!">Consulting Inquiries</a></li>
+                    <li><a href="#!">Media/Press Inquiries</a></li>
+                    <li><a href="#!">Post an event/Opportunity</a></li>
+                    <li><a href="#!">FAQ</a></li>
                 </ul>
             </div>
         </div>
