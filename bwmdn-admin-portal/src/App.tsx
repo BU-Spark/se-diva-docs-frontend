7ea12870b05f7c8d20b9e18ed2eeed008b0c83a1
pdeveau1 <pdeveau@bu.edu>
2023-01-04 12:54:06 -0500

    add the option for posts but adding a posts component and adding it to the app

--- bwmdn-admin-portal/src/App.tsx
+++ bwmdn-admin-portal/src/App.tsx
@@ -1,12 +1,14 @@
-import { Admin, Resource} from "react-admin";
+import { Admin, Resource, ListGuesser} from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { UserList } from "./components/users";
+import { PostList } from "./components/posts";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="users" list={UserList} />
+    <Resource name="posts" list={PostList} />
   </Admin>
 );
 
