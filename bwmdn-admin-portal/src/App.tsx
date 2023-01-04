62fcec04e97b06df8184e71424f8bc04d6e1403d
pdeveau1 <pdeveau@bu.edu>
2023-01-04 12:40:12 -0500

    create a new component UserList instead of ListGuesser

--- bwmdn-admin-portal/src/App.tsx
+++ bwmdn-admin-portal/src/App.tsx
@@ -1,11 +1,12 @@
-import { Admin, Resource, ListGuesser } from "react-admin";
+import { Admin, Resource} from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import { UserList } from "./components/users";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
+    <Resource name="users" list={UserList} />
   </Admin>
 );
 
