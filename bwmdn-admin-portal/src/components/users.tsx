2cc87b81765e1a0eaed833a9b503bf4c01dae982
pdeveau1 <pdeveau@bu.edu>
2023-01-04 12:42:52 -0500

    make the website a urlfield

--- bwmdn-admin-portal/src/components/users.tsx
+++ bwmdn-admin-portal/src/components/users.tsx
@@ -1,4 +1,4 @@
-import { List, Datagrid, TextField, EmailField } from "react-admin";
+import { List, Datagrid, TextField, EmailField, UrlField } from "react-admin";
 
 export const UserList = () => (
   <List>
@@ -9,7 +9,7 @@ export const UserList = () => (
       <EmailField source="email" />
       <TextField source="address.street" />
       <TextField source="phone" />
-      <TextField source="website" />
+      <UrlField source="website" />
       <TextField source="company.name" />
     </Datagrid>
   </List>
