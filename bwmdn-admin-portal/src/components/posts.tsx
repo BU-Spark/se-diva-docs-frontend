1200dbab71af13691befc2360de68213dcc3add3
pdeveau1 <pdeveau@bu.edu>
2023-01-04 12:56:17 -0500

    Add the editing button

--- bwmdn-admin-portal/src/components/posts.tsx
+++ bwmdn-admin-portal/src/components/posts.tsx
@@ -1,12 +1,12 @@
-import { List, Datagrid, TextField, ReferenceField } from "react-admin";
+import { List, Datagrid, TextField, ReferenceField, EditButton } from "react-admin";
 
 export const PostList = () => (
   <List>
-    <Datagrid rowClick="edit">
-      <ReferenceField source="userId" reference="users" />
+    <Datagrid>
       <TextField source="id" />
+      <ReferenceField source="userId" reference="users" />
       <TextField source="title" />
-      <TextField source="body" />
+      <EditButton />
     </Datagrid>
   </List>
 );
