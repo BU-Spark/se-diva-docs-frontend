5a553782bf22177ad12d4baa6281dd795b9bc4ba
pdeveau1 <pdeveau@bu.edu>
2023-01-04 12:54:44 -0500

    change the user number to be their name

--- bwmdn-admin-portal/src/App.tsx
+++ bwmdn-admin-portal/src/App.tsx
@@ -7,7 +7,7 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={UserList} />
+    <Resource name="users" list={UserList} recordRepresentation="name"/>
     <Resource name="posts" list={PostList} />
   </Admin>
 );
