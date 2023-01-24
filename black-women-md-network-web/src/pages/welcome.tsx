4327221a6b2f45d75c51925c89f156534f34dbbd
pdeveau1 <pdeveau@bu.edu>
2023-01-24 12:57:38 -0500

    update text on homepage

--- black-women-md-network-web/src/pages/welcome.tsx
+++ black-women-md-network-web/src/pages/welcome.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import buddies from "../images/test.jpg";
+import buddies from "../images/blackwomenbuddies_modified.jpg";
 import explain from "../images/blackwomanexplains.jpg";
 import "./welcome.css";
 
 const Welcome = () => {
   return (
     <div className="welcome-body">
-      <img className="img-buddies" src = {buddies} alt = "Black Woman Buddies" />
-      <p className="p-buddies">Black Women<br/>M.D. Network</p>
-      <a href="/join" className="btn btn-join btn-lg active" role="button" aria-pressed="true">Join Our Community</a>
-      <p className="p-welcome">Diva Docs Boston is an organization composed of over 250 Black women physicians and 
-      trainees (medical students, residents, and fellows) working, studying or living in Greater Boston. Our members 
-      are represented in every medical speciality and work in a variety of health and non-health care settings.</p>
+      <div className="buddies">
+        <img className="img-buddies" src = {buddies} alt = "Black Woman Buddies" />
+        <p className="p-buddies">Black Women<br/>M.D. Network</p>
+        <a href="/join" className="btn btn-join btn-lg active" role="button" aria-pressed="true">Join Our Community</a>
+      </div>
+      <p className="p-mission">Unapologetically Amplifying Black Women in Medicine!</p>
+      <p className="p-welcome">Diva Docs Boston is an organization composed of over 250 Black women physicians and trainees (medical students, residents, and fellows) working, studying or living in Greater Boston. Our members are represented in every medical speciality and work in a variety of health and non-health care settings.</p>
       <a href="" className="btn btn-members btn-lg active" role="button" aria-pressed="true">Featured Members</a>
       <img className="img-explain" src = {explain} alt = "Black Woman Explains" />
       <p className="p-member">First Name, Last Name<br/>
