18f0eb4f6cd26c09f484e6b81745711b6e30aca1
pdeveau1 <pdeveau@bu.edu>
2023-02-08 10:23:11 -0500

    Restart admin portal

--- bwmdn-admin-portal/src/App.tsx
+++ bwmdn-admin-portal/src/App.tsx
@@ -1,15 +1,26 @@
-import { Admin, Resource, EditGuesser} from "react-admin";
-import jsonServerProvider from "ra-data-json-server";
-import { UserList } from "./components/users";
-import { PostList } from "./components/posts";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-
-const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={UserList} recordRepresentation="name"/>
-    <Resource name="posts" list={PostList} edit={EditGuesser}/>
-  </Admin>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
