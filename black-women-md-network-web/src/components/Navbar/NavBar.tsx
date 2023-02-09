a60707a339a01619be79b435f1199de582e161b3
pdeveau1 <pdeveau@bu.edu>
2023-02-09 10:45:30 -0500

    Please work

--- black-women-md-network-web/src/components/Navbar/NavBar.tsx
+++ black-women-md-network-web/src/components/Navbar/NavBar.tsx
@@ -8,44 +8,59 @@ import Col from 'react-bootstrap/Col'
 
 function NavBar() {
   return (
-    <Navbar className="navbar" variant="dark">
+    <Navbar className="navbar navbar-custom" fixed="top" expand="lg" collapseOnSelect>
       <Container fluid>
-        {/*<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>*/}
+        <Navbar.Brand href="/">BMWDN</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="nav">
-          <Col>
+          <Nav className="me-auto">
+            <NavDropdown className="navdropdown" title="About" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="/whoarewe">Who are We?</NavDropdown.Item>
+              <NavDropdown.Item href="/mission">Our Mission</NavDropdown.Item>
+              <NavDropdown.Item href="/team">Our Team</NavDropdown.Item>
+              <NavDropdown.Item href="/directors">Board of Directors</NavDropdown.Item>
+            </NavDropdown>
             <NavDropdown className="navdropdown" title="Membership" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="/join">Join Us!</NavDropdown.Item>
-              <NavDropdown.Item href="/directory">Directory</NavDropdown.Item>
-              <NavDropdown.Item href="/resumebank">Resume Bank</NavDropdown.Item>
-              <NavDropdown.Item href="/findanexpert">Find An Expert</NavDropdown.Item>
+              <NavDropdown.Item className="disabled"><b>Community</b></NavDropdown.Item>
+              <NavDropdown.Item href="/societies">Member Societies</NavDropdown.Item>
+              <NavDropdown.Item href="/chapters">Member Chapters</NavDropdown.Item>
+              <NavDropdown.Item href="/partners">Partners</NavDropdown.Item>
+              <NavDropdown.Item href="/sponsors">Corporate Sponsors</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item className="disabled"><b>Member Directory</b></NavDropdown.Item>
+              <NavDropdown.Item href="/directory">Search by Last Name</NavDropdown.Item>
+              <NavDropdown.Item href="/directory">Search by Specialty</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item className="disabled"><b>Member Resume Bank</b></NavDropdown.Item>
+              <NavDropdown.Item href="/resumebank">Search by Last Name</NavDropdown.Item>
+              <NavDropdown.Item href="/resumebank">Search by Specialty</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item className="disabled"><b>Networking</b></NavDropdown.Item>
+              <NavDropdown.Item href="/events">Conferences/Meetings/Webinars</NavDropdown.Item>
+              <NavDropdown.Item href="/meals">Meal Gatherings</NavDropdown.Item>
+              <NavDropdown.Item href="/chapterevents">Member Chapter Events</NavDropdown.Item>
             </NavDropdown>
-          </Col>
-          <Col>
             <NavDropdown className="navdropdown" title="Resources" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="/resumereview">Resume Review</NavDropdown.Item>
-              <NavDropdown.Item href="/headshot">Headshot</NavDropdown.Item>
-              <NavDropdown.Item href="/findanexpert">Find An Expert</NavDropdown.Item>
-              <NavDropdown.Item href="/coaching">Coaching</NavDropdown.Item>
+              <NavDropdown.Item href="/jobs">Jobs</NavDropdown.Item>
+              <NavDropdown.Item href="/training">Leadership Training</NavDropdown.Item>
+              <NavDropdown.Item href="/coaching">Physician Coaching</NavDropdown.Item>
+              <NavDropdown.Item href="/opportunities">Other Career Opportunities</NavDropdown.Item>
             </NavDropdown>
-          </Col>
-          <Col>
-            <NavDropdown className="navdropdown" title="Events" id="collasible-nav-dropdown">
+            <NavDropdown className="navdropdown" title="News" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="/blog">Member Blog</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item className="disabled"><b>Member Bulletin Board</b></NavDropdown.Item>
+              <NavDropdown.Item href="/announcements">Announcements</NavDropdown.Item>
+              <NavDropdown.Item href="/community events">Community Events</NavDropdown.Item>
+              <NavDropdown.Item href="/jobs">Jobs/Opportunities</NavDropdown.Item>
+              <NavDropdown.Item href="/question">Post A Question</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/media">Media/Press</NavDropdown.Item>
             </NavDropdown>
-          </Col>
-          <Col>
-            <NavDropdown className="navdropdown" title="Community" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="https://www.divadocsboston.com/">Diva Docs</NavDropdown.Item>
-              <NavDropdown.Item href="/events">Events</NavDropdown.Item>
-            </NavDropdown>
-          </Col>
-          <Col>
+          </Nav>
+          <Nav>
             <Nav.Link href="/signin">Sign In</Nav.Link>
-          </Col>
-          <Col>
             <Nav.Link href="/join">Join</Nav.Link>
-          </Col>
           </Nav>
         </Navbar.Collapse>
       </Container>
