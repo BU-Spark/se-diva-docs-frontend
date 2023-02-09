d07d234188f30e772e2c264d08ea9b0d2d302392
pdeveau1 <pdeveau@bu.edu>
2023-02-09 09:43:25 -0500

    Mock API called in admin portal

--- bwmdn-admin-portal/src/App.tsx
+++ bwmdn-admin-portal/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource, ListGuesser } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const dataProvider = jsonServerProvider('http://localhost:3000');
+
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={ListGuesser} />
+  </Admin>
   );
-}
+  
 
 export default App;
