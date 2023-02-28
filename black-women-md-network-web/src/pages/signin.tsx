f161a0bd6b71a613713c2b0374626e7014af3c60
Mya Turner <68873697+MyaTurner@users.noreply.github.com>
2023-02-28 15:57:33 -0500

    fixed installation issue by creating d.ts files manully (declaring modules after installation

--- black-women-md-network-web/src/pages/signin.tsx
+++ black-women-md-network-web/src/pages/signin.tsx
@@ -1,61 +1,16 @@
-import React, { FormEvent, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import styles from "./signin.module.css";
+import React from "react";
 
 const SignIn = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    console.log(email);
-    console.log(password);
-  };
-
-  let navigate = useNavigate();
-  const routeChange = () => {
-    let path = "/join";
-    navigate(path);
-  };
   return (
-    <div className={styles["outer-container"]}>
-      <div className={styles["auth-form-container"]}>
-        <h2>Login</h2>
-        <form className={styles["login-form"]} onSubmit={handleSubmit}>
-          <label className={styles["label-signin"]} htmlFor="email">
-            Email:{" "}
-          </label>
-
-          <input
-            className={styles["email"]}
-            type="email"
-            placeholder="Email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-
-          <label className={styles["label-signin"]} htmlFor="password">
-            Password:{" "}
-          </label>
-
-          <input
-            className={styles["password"]}
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button className={styles["link-btn"]}>Forgot password?</button>
-          <button className={styles["submit"]} type="submit">
-            Log In
-          </button>
-        </form>
-        <button className={styles["link-btn"]} onClick={routeChange}>
-          Not apart of the Black Women M.D. Network? Become a member here.
-        </button>
-      </div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "Right",
+        alignItems: "Right",
+        height: "100vh",
+      }}
+    >
+      <h1>Sign Up</h1>
     </div>
   );
 };
