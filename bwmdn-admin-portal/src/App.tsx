40f0cc86f9d84fcc63228c6023ea6b75a192ac6b
pdeveau1 <pdeveau@bu.edu>
2023-02-28 09:19:52 -0500

    just in case

--- bwmdn-admin-portal/src/App.tsx
+++ bwmdn-admin-portal/src/App.tsx
@@ -1,11 +1,16 @@
-import { Admin, Resource, ShowGuesser } from "react-admin";
+import { Admin, Resource, ShowGuesser, EditGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { UserList, UserEdit } from "./components/users";
 //import simpleRestProvider from 'ra-data-simple-rest';
 
-//const dataProvider = jsonServerProvider('http://localhost:3000');
+
+
+
+
 const dataProvider = jsonServerProvider('https://se-diva-docs.herokuapp.com');
 //const dataProvider = simpleRestProvider('https://se-diva-docs.herokuapp.com');
+console.log(dataProvider)
+
 const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="applicants/view" list={UserList} edit={UserEdit} show={ShowGuesser}/>
