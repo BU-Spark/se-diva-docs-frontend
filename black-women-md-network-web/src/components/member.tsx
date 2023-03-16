46d01bb1f939197a7c647e536f63fbb938d3846e
pdeveau1 <pdeveau@bu.edu>
2023-03-16 10:44:15 -0400

    directory using API call and bootstrap cards

--- black-women-md-network-web/src/components/member.tsx
+++ black-women-md-network-web/src/components/member.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 interface MemberProps {
-  name: string;
-  email: string;
-  phone: string;
+  id: string;
+  first_name: string;
+  last_name: string;
 }
 
-const Member: React.FC<MemberProps> = ({ name, email, phone }) => {
-  return (
+const Member: React.FC<MemberProps> = ({ id, first_name, last_name }) => {
+  /*return (
     <div>
-      <h2>{name}</h2>
-      <p>Email: {email}</p>
-      <p>Phone: {phone}</p>
+      <h2>{id}</h2>
+      <p>Email: {first_name}</p>
+      <p>Phone: {last_name}</p>
     </div>
+  );*/
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{first_name} {last_name}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
   );
 };
 
--- black-women-md-network-web/src/pages/directory.tsx
+++ black-women-md-network-web/src/pages/directory.tsx
@@ -1,38 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { fetchMembers } from '../components/api';
-import Member from '../components/member';
+import { Card, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./directory.css";
 
 interface MemberData {
-    id: string;
+    id: number;
     first_name: string;
     last_name: string;
+    primary_email: string;
+    phone_number: string;
   }
 
-const Directory: React.FC = () => {
-  const [members, setMembers] = useState<MemberData[]>([]);
+  const MemberCard: React.FC<MemberData> = ({ id, first_name, last_name, primary_email, phone_number }) => {
+    return (
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>{first_name} {last_name}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">{primary_email}</Card.Subtitle>
+          <Card.Text>{phone_number}</Card.Text>
+          <Button variant="primary">View Details</Button>
+        </Card.Body>
+      </Card>
+    );
+  };
 
-  useEffect(() => {
-    async function getMembers() {
-      const data = await fetchMembers();
-      setMembers(data);
-    }
+  const Directory: React.FC = () => {
+    const [members, setMembers] = useState<MemberData[]>([]);
+  
+    useEffect(() => {
+      const fetchMembers = async () => {
+        const response = await fetch('https://se-diva-docs.herokuapp.com/approvedapplicants/view');
+        const data = await response.json();
+        setMembers(data);
+      };
+      fetchMembers();
+    }, []);
+  
+    return (
+      <div className="d-flex flex-wrap justify-content-center">
+        {members.map((member) => (
+          <MemberCard key={member.id} {...member} />
+        ))}
+      </div>
+    );
+  };
+  
 
-    getMembers();
-  }, []);
-
-  return (
-    <div>
-      <h1>Membership Directory</h1>
-      {members.map((member, index) => (
-        <Member
-          key={index}
-          name={member.id}
-          email={member.first_name}
-          phone={member.last_name}
-        />
-      ))}
-    </div>
-  );
-};
-
-export default Directory;
+  export default Directory;
+  
