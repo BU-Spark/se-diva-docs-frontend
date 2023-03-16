6e0b61672bbbecbc1de0eee38eda61f54663545c
pdeveau1 <pdeveau@bu.edu>
2023-03-16 12:57:56 -0400

    Add check box to filter by connection and resume

--- black-women-md-network-web/src/pages/directory.tsx
+++ black-women-md-network-web/src/pages/directory.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Badge } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./directory.css";
 
@@ -9,15 +9,30 @@ interface MemberData {
     last_name: string;
     primary_email: string;
     phone_number: string;
+    resume_included_question: string;
+    will_sponsor_question: {
+        sponsor_question_answer: String,
+        activities_interested: []
+      };
   }
 
-  const MemberCard: React.FC<MemberData> = ({ id, first_name, last_name, primary_email, phone_number }) => {
+  const MemberCard: React.FC<MemberData> = ({ id, first_name, last_name, primary_email, phone_number, resume_included_question }) => {
     return (
       <Card style={{ width: '18rem' }}>
         <Card.Body>
-          <Card.Title>{first_name} {last_name}</Card.Title>
+          <Card.Title>{last_name}, {first_name}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">{primary_email}</Card.Subtitle>
           <Card.Text>{phone_number}</Card.Text>
+          {resume_included_question.toLowerCase() == "yes" && (
+            <Badge pill bg="success">
+              Resume Available
+            </Badge>
+          )}
+          {resume_included_question.toLowerCase() == "no" && (
+            <Badge pill bg="secondary">
+              Resume Not Available
+            </Badge>
+          )}
           <Button variant="primary">View Details</Button>
         </Card.Body>
       </Card>
@@ -25,9 +40,12 @@ interface MemberData {
   };
   
   
+  
   const Directory: React.FC = () => {
     const [members, setMembers] = useState<MemberData[]>([]);
     const [searchTerm, setSearchTerm] = useState<string>('');
+    const [showResumeOnly, setShowResumeOnly] = useState<boolean>(false);
+    const [showConnectionOnly, setShowConnectionOnly] = useState<boolean>(false);
   
     useEffect(() => {
       const fetchMembers = async () => {
@@ -38,9 +56,12 @@ interface MemberData {
       fetchMembers();
     }, []);
   
-    const filteredMembers = members.filter((member) =>
-      member.first_name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    const filteredMembers = members
+      .filter((member) =>
+        member.first_name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .filter((member) => (!showResumeOnly || member.resume_included_question.toLowerCase() == "yes"))
+      .filter((member) => (!showConnectionOnly || member.will_sponsor_question.sponsor_question_answer.toLowerCase() == "yes"));
   
     return (
       <div>
@@ -53,6 +74,24 @@ interface MemberData {
             onChange={(e) => setSearchTerm(e.target.value)}
           />
         </div>
+        <div className="form-group form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            checked={showResumeOnly}
+            onChange={(e) => setShowResumeOnly(e.target.checked)}
+          />
+          <label className="form-check-label">Resume Available</label>
+        </div>
+        <div className="form-group form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            checked={showConnectionOnly}
+            onChange={(e) => setShowConnectionOnly(e.target.checked)}
+          />
+          <label className="form-check-label">Open to Connect</label>
+        </div>
         <div className="d-flex flex-wrap justify-content-center">
           {filteredMembers.map((member) => (
             <MemberCard key={member.id} {...member} />
@@ -61,6 +100,7 @@ interface MemberData {
       </div>
     );
   };
+  
   
 
   export default Directory;
