b688f61f6abb770a05abfca06f95cdd78b91e724
pdeveau1 <pdeveau@bu.edu>
2023-04-12 00:07:01 -0400

    Update footer for if signed in or not

--- black-women-md-network-web/src/components/Footer/Footer.tsx
+++ black-women-md-network-web/src/components/Footer/Footer.tsx
@@ -2,25 +2,56 @@ import React from "react";
 import logo from "./BlackWomenMDNetworkLogo1.jpg";
 import twitter from "./twitter.jpg";
 import "./Footer.css";
+import { useIsAuthenticated, useSignOut } from "react-auth-kit";
+import { useNavigate } from "react-router-dom";
 
-const Footer = () => (
-  <div className="footer">
-    <div className="footer-links">
-      <a href="/">BWMDN</a>
-      <a href="/about">About</a>
-      <a href="/members">Membership</a>
-      <a href="/resources">Resources</a>
-      <a href="/news">News</a>
-    </div>
-    <div className="footer-image">
-      <img className="logo-img" src={logo} alt="" />
-    </div>
-    <div className="footer-contact">
-      <a href="/">Contact Us</a>
-      <a href="/">Follow Us:</a>
-      <img className="twitter-img" src={twitter} alt="" />
-    </div>
-  </div>
-);
+function Footer() {
+  const signOut = useSignOut();
+  const navigate = useNavigate();
+  const isAuthenticated = useIsAuthenticated();
+  console.log("is the user authed? " + isAuthenticated());
+
+  const logOut = () => {
+    signOut();
+    navigate("/signin");
+  };
+  if (isAuthenticated()) {
+    return (
+      <div className="footer">
+        <div className="footer-links">
+          <a href="/">BWMDN</a>
+          <a href="/about">About</a>
+          <a href="/members">Membership</a>
+          <a href="/resources">Resources</a>
+          <a href="/news">News</a>
+        </div>
+        <div className="footer-image">
+          <img className="logo-img" src={logo} alt="" />
+        </div>
+        <div className="footer-contact">
+          <a href="/">Contact Us</a>
+          <a href="/">Follow Us:</a>
+          <img className="twitter-img" src={twitter} alt="" />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="footer">
+        <div className="footer-links">
+          <a href="/">BWMDN</a>
+        </div>
+        <div className="footer-image">
+          <img className="logo-img" src={logo} alt="" />
+        </div>
+        <div className="footer-contact">
+          <a href="/">Contact Us</a>
+          <a href="/">Follow Us:</a>
+          <img className="twitter-img" src={twitter} alt="" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Footer;
