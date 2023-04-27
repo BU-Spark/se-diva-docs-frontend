bc05b564920828ac0762ed402478f3850a00e843
Mya Turner <68873697+MyaTurner@users.noreply.github.com>
2023-04-27 10:26:45 -0400

    deleted second package.json

---
+++ bwmdn-admin-portal/src/authprovider.tsx
@@ -0,0 +1,35 @@
+import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR } from 'react-admin';
+
+export default (type: any, params: any) => {
+    if (type === AUTH_LOGIN) {
+        const { username, password } = params;
+        const request = new Request('https://mydomain.com/authenticate', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+        })
+        return fetch(request)
+            .then(response => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(({ token }) => {
+                localStorage.setItem('token', token);
+            });
+    }
+    if (type === AUTH_LOGOUT) {
+        localStorage.removeItem('token');
+        return Promise.resolve();
+    }
+    if (type === AUTH_ERROR) {
+        const status  = params.status;
+        if (status === 401 || status === 403) {
+            localStorage.removeItem('token');
+            return Promise.reject();
+        }
+        return Promise.resolve();
+    }
+    return Promise.resolve();
+};
